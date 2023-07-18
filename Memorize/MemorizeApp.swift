4d8ad3f861a8fab9de0c3ebfdb9fdc1c661aca23
vyatkinsen <vyatkinsen@bk.ru>
2023-07-18 13:34:35 +0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Арсений Вяткин on 18.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
